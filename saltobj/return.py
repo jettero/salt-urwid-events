6ed5dd14fee6e5f6bed72d7ec605387a5b901ca8
Paul Miller <jettero@gmail.com>
2017-02-11 12:45:58 -0500

    notes and examples

--- saltobj/return.py
+++ saltobj/return.py
@@ -16,6 +16,8 @@ class Return(object):
 
         # XXX: the examples below also show a re-written result, data-in-data is removed already
         #      what does state.event pretty do to refornicate the data??
+        #      collected examples of syndication... looks like we receive the events multiple times
+        #      the final form has the syndic info stripped off already
 
     @property
     def 
@@ -23,6 +25,247 @@ class Return(object):
     @property
     def is_syndicated(self):
         return bool( self.syn )
+
+### examples from experimental/no-urwid-events.py
+# {
+#   "tag": "20170211093753973385", 
+#   "data": {
+#     "_stamp": "2017-02-11T17:37:53.974017", 
+#     "minions": []
+#   }
+# } 
+
+# {
+#   "tag": "salt/job/20170211093753973385/new", 
+#   "data": {
+#     "tgt_type": "glob", 
+#     "jid": "20170211093753973385", 
+#     "tgt": "host00.dc2", 
+#     "_stamp": "2017-02-11T17:37:53.974437", 
+#     "user": "sudo_jettero", 
+#     "arg": [
+#       "role"
+#     ], 
+#     "fun": "grains.get", 
+#     "minions": []
+#   }
+# } 
+
+# {
+#   "tag": "20170211093753973385", 
+#   "data": {
+#     "_stamp": "2017-02-11T17:37:54.321302", 
+#     "tag": "20170211093753973385", 
+#     "data": {
+#       "_stamp": "2017-02-11T17:37:54.249523", 
+#       "minions": []
+#     }
+#   }
+# } 
+
+# {
+#   "tag": "syndic/salt.stage.or1:b91edb59-f18c-9a7d-5af4-c28358f401fb/20170211093753973385", 
+#   "data": {
+#     "_stamp": "2017-02-11T17:37:54.321722", 
+#     "minions": []
+#   }
+# } 
+
+# {
+#   "tag": "salt/job/20170211093753973385/new", 
+#   "data": {
+#     "_stamp": "2017-02-11T17:37:54.322040", 
+#     "tag": "salt/job/20170211093753973385/new", 
+#     "data": {
+#       "tgt_type": "glob", 
+#       "jid": "20170211093753973385", 
+#       "tgt": "host00.dc2", 
+#       "_stamp": "2017-02-11T17:37:54.249680", 
+#       "user": "sudo_jettero", 
+#       "arg": [
+#         "role"
+#       ], 
+#       "fun": "grains.get", 
+#       "minions": []
+#     }
+#   }
+# } 
+
+# {
+#   "tag": "syndic/salt.stage.or1:b91edb59-f18c-9a7d-5af4-c28358f401fb/salt/job/20170211093753973385/new", 
+#   "data": {
+#     "tgt_type": "glob", 
+#     "jid": "20170211093753973385", 
+#     "tgt": "host00.dc2", 
+#     "_stamp": "2017-02-11T17:37:54.322445", 
+#     "user": "sudo_jettero", 
+#     "arg": [
+#       "role"
+#     ], 
+#     "fun": "grains.get", 
+#     "minions": []
+#   }
+# } 
+
+# {
+#   "tag": "20170211093753973385", 
+#   "data": {
+#     "_stamp": "2017-02-11T17:37:55.103770", 
+#     "tag": "20170211093753973385", 
+#     "data": {
+#       "_stamp": "2017-02-11T17:37:54.396075", 
+#       "minions": []
+#     }
+#   }
+# } 
+
+# {
+#   "tag": "syndic/saltmaster.dc5/20170211093753973385", 
+#   "data": {
+#     "_stamp": "2017-02-11T17:37:55.104137", 
+#     "minions": []
+#   }
+# } 
+
+# {
+#   "tag": "salt/job/20170211093753973385/new", 
+#   "data": {
+#     "_stamp": "2017-02-11T17:37:55.104408", 
+#     "tag": "salt/job/20170211093753973385/new", 
+#     "data": {
+#       "tgt_type": "glob", 
+#       "jid": "20170211093753973385", 
+#       "tgt": "host00.dc2", 
+#       "_stamp": "2017-02-11T17:37:54.397843", 
+#       "user": "sudo_jettero", 
+#       "arg": [
+#         "role"
+#       ], 
+#       "fun": "grains.get", 
+#       "minions": []
+#     }
+#   }
+# } 
+
+# {
+#   "tag": "syndic/saltmaster.dc5/salt/job/20170211093753973385/new", 
+#   "data": {
+#     "tgt_type": "glob", 
+#     "jid": "20170211093753973385", 
+#     "tgt": "host00.dc2", 
+#     "_stamp": "2017-02-11T17:37:55.104706", 
+#     "user": "sudo_jettero", 
+#     "arg": [
+#       "role"
+#     ], 
+#     "fun": "grains.get", 
+#     "minions": []
+#   }
+# } 
+
+# {
+#   "tag": "20170211093753973385", 
+#   "data": {
+#     "_stamp": "2017-02-11T17:37:55.456486", 
+#     "tag": "20170211093753973385", 
+#     "data": {
+#       "_stamp": "2017-02-11T17:37:54.357814", 
+#       "minions": [
+#         "host00.dc2""
+#       ]
+#     }
+#   }
+# } 
+
+# {
+#   "tag": "syndic/saltmaster.dc2/20170211093753973385", 
+#   "data": {
+#     "_stamp": "2017-02-11T17:37:55.456856", 
+#     "minions": [
+#       "host00.dc2""
+#     ]
+#   }
+# } 
+
+# {
+#   "tag": "20170211093753973385", 
+#   "data": {
+#     "_stamp": "2017-02-11T17:37:55.456890", 
+#     "tag": "20170211093753973385", 
+#     "data": {
+#       "_stamp": "2017-02-11T17:37:54.351020", 
+#       "minions": []
+#     }
+#   }
+# } 
+
+# {
+#   "tag": "salt/job/20170211093753973385/new", 
+#   "data": {
+#     "_stamp": "2017-02-11T17:37:55.457123", 
+#     "tag": "salt/job/20170211093753973385/new", 
+#     "data": {
+#       "tgt_type": "glob", 
+#       "jid": "20170211093753973385", 
+#       "tgt": "host00.dc2", 
+#       "_stamp": "2017-02-11T17:37:54.358251", 
+#       "user": "sudo_jettero", 
+#       "arg": [
+#         "role"
+#       ], 
+#       "fun": "grains.get", 
+#       "minions": [
+#         "host00.dc2""
+#       ]
+#     }
+#   }
+# } 
+
+# {
+#   "tag": "syndic/saltmaster.dc3/20170211093753973385", 
+#   "data": {
+#     "_stamp": "2017-02-11T17:37:55.457289", 
+#     "minions": []
+#   }
+# } 
+
+# {
+#   "tag": "syndic/saltmaster.dc2/salt/job/20170211093753973385/new", 
+#   "data": {
+#     "tgt_type": "glob", 
+#     "jid": "20170211093753973385", 
+#     "tgt": "host00.dc2", 
+#     "_stamp": "2017-02-11T17:37:55.457439", 
+#     "user": "sudo_jettero", 
+#     "arg": [
+#       "role"
+#     ], 
+#     "fun": "grains.get", 
+#     "minions": [
+#       "host00.dc2""
+#     ]
+#   }
+# } 
+
+# {
+#   "tag": "salt/job/20170211093753973385/new", 
+#   "data": {
+#     "_stamp": "2017-02-11T17:37:55.457563", 
+#     "tag": "salt/job/20170211093753973385/new", 
+#     "data": {
+#       "tgt_type": "glob", 
+#       "jid": "20170211093753973385", 
+#       "tgt": "host00.dc2", 
+#       "_stamp": "2017-02-11T17:37:54.352037", 
+#       "user": "sudo_jettero", 
+#       "arg": [
+#         "role"
+#       ], 
+#       "fun": "grains.get", 
+#       "minions": []
+#     }
+#   }
+# } 
 
 ### from salt-run state.event pretty=true, not how the data is really presented.
 # Data really arrives with an outer wrapper dict with 'tag' and 'data' (which
