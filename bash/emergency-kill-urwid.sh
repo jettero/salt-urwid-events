73a51f07c92203520bc8456c868587a043ef0320
Paul Miller <jettero@gmail.com>
2017-02-18 18:11:40 -0500

    we're now called uevents... so

--- bash/emergency-kill-urwid.sh
+++ bash/emergency-kill-urwid.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 function newk_urwid() {
-    pgrep -f python.*urwid | sudo xargs -trn1 kill -2
+    pgrep -f python.*urwid  | sudo xargs -trn1 kill -2
+    pgrep -f python.*uevent | sudo xargs -trn1 kill -2
 }
