480300edf5ccfd5859f5278796fc40fd3c52c5ee
Paul Miller <jettero@gmail.com>
2017-02-25 08:58:28 -0500

    new newk

--- bash/emergency-kill-urwid.sh
+++ bash/emergency-kill-urwid.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 function newk_urwid() {
-    pgrep -f python.*urwid  | sudo xargs -trn1 kill -2
-    pgrep -f python.*uevent | sudo xargs -trn1 kill -2
+    for i in 2 15 9; do
+        pgrep -f python.*urwid  | sudo xargs -trn1 kill -$i
+        pgrep -f python.*uevent | sudo xargs -trn1 kill -$i
+    done
 }
