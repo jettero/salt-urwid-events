ad89952c55f5f501b2dd5faf010792110f3124e7
Paul Miller <jettero@gmail.com>
2017-03-08 09:02:28 -0500

    note to others

--- bash/emergency-kill-urwid.sh
+++ bash/emergency-kill-urwid.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# It comes up surprisingly frequently that the forking backend goes insane and
+# takes down my whole aws instance for a while (until the OOM gets to it).
+# this helps to shut the beast down until I find whatever the next bug is.
+
 function newk_urwid() {
     for i in 2 15 9; do
         pgrep -f python.*urwid  | sudo xargs -trn1 kill -$i
