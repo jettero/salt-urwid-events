8ebfa2c7a6438573b91c089b85b0b8ef38778c02
Paul Miller <jettero@gmail.com>
2017-03-20 12:27:13 -0400

    ahh, I think I see the problem. Expected returns don't have jids exactly. Maybe related.

--- note/find-job-example.js
+++ note/find-job-example.js
@@ -2,14 +2,23 @@
 // salt host.dc1 saltutil.find_job 20170320113504719477
 
 {
-  "tag": "salt/job/20170320113601041969/new", 
+  "tag": "20170320122537669665", 
   "data": {
-    "tgt_type": "list", 
-    "jid": "20170320113601041969", 
-    "tgt": [
+    "_stamp": "2017-03-20T16:25:37.669916", 
+    "minions": [
       "host.dc1"
-    ], 
-    "_stamp": "2017-03-20T15:36:01.043720", 
+    ]
+  }, 
+  "_evno": 0
+}
+
+{
+  "tag": "salt/job/20170320122537669665/new", 
+  "data": {
+    "tgt_type": "glob", 
+    "jid": "20170320122537669665", 
+    "tgt": "host.dc1", 
+    "_stamp": "2017-03-20T16:25:37.670350", 
     "user": "sudo_jettero", 
     "arg": [
       "20170320113504719477"
@@ -19,23 +28,21 @@
       "host.dc1"
     ]
   }, 
-  "_evno": 25
+  "_evno": 1
 }
 
-// and the return (technically of a later query... don't compare the times)
-
 {
-  "tag": "salt/job/20170320113925697924/ret/host.dc1", 
+  "tag": "salt/job/20170320122537669665/ret/host.dc1", 
   "data": {
     "fun_args": [
       "20170320113504719477"
     ], 
-    "jid": "20170320113925697924", 
+    "jid": "20170320122537669665", 
     "return": {}, 
     "retcode": 0, 
     "success": true, 
     "cmd": "_return", 
-    "_stamp": "2017-03-20T15:39:26.152602", 
+    "_stamp": "2017-03-20T16:25:38.057041", 
     "fun": "saltutil.find_job", 
     "id": "host.dc1"
   }, 
