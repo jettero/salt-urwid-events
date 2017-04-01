039988e78614b18eb8f76d9765afd555b1e0aded
Paul Miller <jettero@gmail.com>
2017-04-01 15:59:44 -0400

    I'm not sure if this'll work... my thinking is:
    ayt is yellow (even when bolded)
    failed is red (bold red when there's changes)
    succeeded is green (bold green when changed)
    
    … ja??

--- urwidobj/pallet.py
+++ urwidobj/pallet.py
@@ -5,6 +5,11 @@ _PALLET = {
     'main': {
         'status':   (     'yellow', 'dark blue'),
         'selected': ( 'light blue',   'default'),
+
+        'ayt':       ( 'yellow', 'default' ),
+        'failed':    ( 'red',    'default' ),
+        'succeeded': ( 'green',  'default' ),
+        'changes':   ( 'bold',   'default' ),
     }
 }
 
