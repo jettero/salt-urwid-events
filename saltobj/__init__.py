a2b79a151ac864ea0c7e8b97b252e57dff7b3258
Paul Miller <jettero@gmail.com>
2017-04-09 09:20:11 -0400

    make generators in saltobj for reading files (hides the use of ForkedSaltPipeWriter, while still using it

--- saltobj/__init__.py
+++ saltobj/__init__.py
@@ -1,3 +1,14 @@
 
 from event import JidCollector, classify_event
 from forkedsalt import ForkedSaltPipeWriter, RS
+
+def read_event_file(fname):
+    fspw = ForkedSaltPipeWriter(replay_file=fname, replay_only=True)
+    while True:
+        evj = fspw.next()
+        if evj: yield evj
+        else: break
+
+def classify_event_file(fname):
+    for evj in read_event_file(fname):
+        yield classify_event(evj)
--- t/conftest.py
+++ t/conftest.py
@@ -2,15 +2,6 @@
 import pytest
 import saltobj
 
-def _replay_file(fname):
-    fspw = saltobj.ForkedSaltPipeWriter(replay_file=fname, replay_only=True)
-    ret = []
-    evj = fspw.next()
-    while evj:
-        ret.append(evj)
-        evj = fspw.next()
-    return ret
-
 @pytest.fixture
 def pinglog_json():
-    return _replay_file('t/_ping.log')
+    return list(saltobj.read_event_file('t/_ping.log'))
