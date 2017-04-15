502830eb6826b841ae7ad98065252abb9327c13a
Paul Miller <jettero@gmail.com>
2017-04-15 08:44:15 -0400

    attempt to re-use more of the parent mfl

--- misc/mfl.py
+++ misc/mfl.py
@@ -1,38 +1,33 @@
 # coding: utf-8
 
-import urwid.monitored_list
 import urwid.listbox
+from urwid.monitored_list import MonitoredFocusList as pMFL
 
 # /usr/lib/python2.7/site-packages/urwid/monitored_list.py
 
 class MyFocusListMixin(object):
+    def _set_focus(self, index):
+        if not self: return
+        index = index % len(self)
+        super(MyFocusListMixin, self)._set_focus(index)
+
+    focus = property(pMFL._get_focus, _set_focus)
+
     @property
     def cur(self):
         if not self:
             return
-        return self[ self._focus ]
+        return self[ self.focus ]
 
     @property
     def next(self):
-        if not self: return
-        self.pos += 1
+        self.focus += 1
         return self.cur
 
     @property
     def prev(self):
-        if not self: return
-        self.pos -= 1
+        self.focus -= 1
         return self.cur
-
-    @property
-    def pos(self):
-        if not self: return
-        return self._focus % len(self)
-
-    @pos.setter
-    def pos(self, v):
-        if not self: return
-        self._focus = v % len(self)
 
 class MyFocusList(urwid.monitored_list.MonitoredFocusList, MyFocusListMixin):
     pass
