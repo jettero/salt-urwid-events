00447be55778adcd945ebe953ce70454cd58f924
Paul Miller <jettero@gmail.com>
2017-04-15 13:53:38 -0400

    test this focus shit

--- t/test_mfl.py
+++ t/test_mfl.py
@@ -47,3 +47,25 @@ def test_babysit_list():
     assert len(sflw) == 1 and sflw[0] == item
     item = mfl.next
     assert len(sflw) == 1 and sflw[0] == item
+
+def test_focus():
+    class hrm():
+        focused = False
+        def set_focused(self):
+            self.focused = True
+        def set_unfocused(self):
+            self.focused = False
+        def __repr__(self):
+            return "hrm[focused={0}]".format(self.focused)
+
+    mfl = MyFocusList()
+    mfl.append(hrm())
+    mfl.append(hrm())
+
+    assert mfl.focus == 0
+    assert     mfl[0].focused
+    assert not mfl[1].focused
+
+    mfl.focus = 1
+    assert not mfl[0].focused
+    assert     mfl[1].focused
