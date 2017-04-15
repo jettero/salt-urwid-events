60855beb25b45763141df2bda2cde194f2f7f2ba
Paul Miller <jettero@gmail.com>
2017-04-15 08:05:35 -0400

    redo lwp with mfl

--- misc/list_ptr.py
+++ misc/list_ptr.py
@@ -2,59 +2,33 @@
 
 import urwid.monitored_list
 
-# I think this might be partially re-inventing-the-wheel …
-# isn't this what list walkers are for?
+# /usr/lib/python2.7/site-packages/urwid/monitored_list.py
 
-class ListWithPtr(urwid.monitored_list.MonitoredList):
-    _pos = -1
-    siv  = []
-
-    def __init__(self, *a, **kw):
-        super(ListWithPtr,self).__init__(*a, **kw)
-        self._modified()
-
-    def _modified(self):
-        if not self:
-            self._pos = -1
-            while self.siv:
-                self.siv.pop()
-
-        elif not self.siv or self.siv[0] is not self.cur:
-            while self.siv:
-                self.siv.pop()
-            self.siv.append(self.cur)
-            
+class ListWithPtr(urwid.monitored_list.MonitoredFocusList):
     @property
     def cur(self):
-        try: return self[ self.pos ]
-        except: pass
+        if not self:
+            return
+        return self[ self._focus ]
 
     @property
     def next(self):
-        try: self.pos += 1
-        except: pass
-        self._modified()
+        if not self: return
+        self.pos += 1
         return self.cur
 
     @property
     def prev(self):
-        try: self.pos -= 1
-        except: pass
-        self._modified()
+        if not self: return
+        self.pos -= 1
         return self.cur
 
     @property
     def pos(self):
-        try: return self._pos % len(self)
-        except: pass
+        if not self: return
+        return self._focus % len(self)
 
     @pos.setter
     def pos(self, v):
-        self._pos = v
-        self._modified()
-
-    def __repr__(self):
-        p = self.pos
-        return '[' + (' '.join([
-            ('<{0}>' if i == p else '{0}').format(v) for i,v in enumerate(self)
-        ])) + ']'
+        if not self: return
+        self._focus = v % len(self)
