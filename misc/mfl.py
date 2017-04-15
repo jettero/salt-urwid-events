51b69f690d378b66cce7b473bfe92c5e547092d9
Paul Miller <jettero@gmail.com>
2017-04-15 10:27:34 -0400

    I didn't really understand one of the jobs ListPtr was doing...

--- misc/mfl.py
+++ misc/mfl.py
@@ -1,11 +1,10 @@
 # coding: utf-8
 
-import urwid.listbox
 import urwid.monitored_list
 
 # /usr/lib/python2.7/site-packages/urwid/monitored_list.py
 
-class MyFocusListMixin(urwid.monitored_list.MonitoredFocusList):
+class MyFocusList(urwid.monitored_list.MonitoredFocusList):
     auto_follow = False
 
     def __init__(self, *a, **kw):
@@ -16,14 +15,14 @@ class MyFocusListMixin(urwid.monitored_list.MonitoredFocusList):
             self.auto_follow = bool(kw.pop('follow'))
         if 'focus' in kw:
             set_focus = kw.pop('focus')
-        super(MyFocusListMixin, self).__init__(*a, **kw)
+        super(MyFocusList, self).__init__(*a, **kw)
         if set_focus is not None:
             if set_focus == -1:
                 self.auto_follow = True
             self.focus = set_focus
 
     def _adjust_focus_on_contents_modified(self, slc, new_items=()):
-        focus = super(MyFocusListMixin, self)._adjust_focus_on_contents_modified(slc, new_items=new_items)
+        focus = super(MyFocusList, self)._adjust_focus_on_contents_modified(slc, new_items=new_items)
         if self.auto_follow and new_items:
             if focus == len(self)-1:
                 focus += len(new_items)
@@ -32,10 +31,10 @@ class MyFocusListMixin(urwid.monitored_list.MonitoredFocusList):
     def _set_focus(self, index):
         if not self: return
         index = index % len(self)
-        super(MyFocusListMixin,self)._set_focus(index)
+        super(MyFocusList,self)._set_focus(index)
 
     def _get_focus(self):
-        return super(MyFocusListMixin,self)._get_focus()
+        return super(MyFocusList,self)._get_focus()
 
     focus = property(_get_focus, _set_focus)
 
@@ -54,9 +53,3 @@ class MyFocusListMixin(urwid.monitored_list.MonitoredFocusList):
     def prev(self):
         self.focus -= 1
         return self.cur
-
-class MyFocusList(MyFocusListMixin):
-    pass
-
-class MySimpleFocusListWalker(MyFocusListMixin, urwid.listbox.SimpleFocusListWalker):
-    pass
