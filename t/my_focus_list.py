c936dbe1f5ffb87fe7012de5aac07f4242aaab53
Paul Miller <jettero@gmail.com>
2017-04-15 08:10:25 -0400

    focus list tests

---
+++ t/my_focus_list.py
@@ -0,0 +1,22 @@
+
+from misc.list_ptr import ListWithPtr
+
+def test_lwp()
+    lwp1 = ListWithPtr([1,2,3,4])
+    assert lwp1.pos == 0
+    lwp1.pos = -1
+    assert lwp1.pos == len(lwp1)-1
+    assert lwp1.cur == 4
+    assert lwp1.next == 1
+    assert lwp1.next == 2
+    assert lwp1.next == 3
+    assert lwp1.next == 4
+    assert lwp1.next == 1
+    assert lwp1.prev == 4
+    assert lwp1.prev == 3
+    assert lwp1.prev == 2
+    assert lwp1.prev == 1
+    assert lwp1.prev == 4
+    assert lwp1.cur == 4
+    assert lwp1.cur == 4
+    assert lwp1.cur == 4
