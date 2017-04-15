e8a0031039a6b7bbcc8565702559ff79da8da313
Paul Miller <jettero@gmail.com>
2017-04-15 09:13:28 -0400

    lwp makes no sense anymore

--- t/test_mfl.py
+++ t/test_mfl.py
@@ -2,24 +2,24 @@
 from misc.mfl import MyFocusList, MySimpleFocusListWalker
 
 def test_mfl(test_class=MyFocusList):
-    lwp1 = test_class([1,2,3,4])
-    assert lwp1.focus == 0
-    lwp1.focus = -1
-    assert lwp1.focus == len(lwp1)-1
-    assert lwp1.cur == 4
-    assert lwp1.next == 1
-    assert lwp1.next == 2
-    assert lwp1.next == 3
-    assert lwp1.next == 4
-    assert lwp1.next == 1
-    assert lwp1.prev == 4
-    assert lwp1.prev == 3
-    assert lwp1.prev == 2
-    assert lwp1.prev == 1
-    assert lwp1.prev == 4
-    assert lwp1.cur == 4
-    assert lwp1.cur == 4
-    assert lwp1.cur == 4
+    l = test_class([1,2,3,4])
+    assert l.focus == 0
+    l.focus = -1
+    assert l.focus == len(l)-1
+    assert l.cur == 4
+    assert l.next == 1
+    assert l.next == 2
+    assert l.next == 3
+    assert l.next == 4
+    assert l.next == 1
+    assert l.prev == 4
+    assert l.prev == 3
+    assert l.prev == 2
+    assert l.prev == 1
+    assert l.prev == 4
+    assert l.cur == 4
+    assert l.cur == 4
+    assert l.cur == 4
 
 def test_msflw():
     test_mfl( MySimpleFocusListWalker )
