67f6057505d4f688a8b8fd0c9abb8da6346a71fe
Paul Miller <jettero@gmail.com>
2017-06-14 07:31:32 -0400

    limits are too low to run the thing

--- limit-shell.sh
+++ limit-shell.sh
@@ -1,6 +1,6 @@
 #!/source/this/bash
 
-declare -A limits
+set -x
 
 ulimit -u 80 # simple minor bugs in the forking have crashed my dev host,
              # trigging ridiculous oom decisions
@@ -9,28 +9,25 @@ ulimit -u 80 # simple minor bugs in the forking have crashed my dev host,
 
 # I added these off the top of my head as resonably large numbers that will
 # still leave the system salvagable after a fork-bomb accident (maybe)
-ulimit -c 1000
-ulimit -e 15
-ulimit -d 1500
-ulimit -m 2500
-ulimit -t 3600
-ulimit -v 2000
 
-# core file size          (blocks, -c) 0
-# data seg size           (kbytes, -d) unlimited
-# scheduling priority             (-e) 0
+ulimit -c 1000  # core file size          (blocks, -c) 0
+ulimit -e 15    # scheduling priority             (-e) 0
+ulimit -d 3000  # data seg size           (kbytes, -d) unlimited
+ulimit -m 3000  # max memory size         (kbytes, -m) unlimited
+ulimit -t 3600  # cpu time               (seconds, -t) unlimited
+ulimit -v 5000  # virtual memory          (kbytes, -v) unlimited
+
+set +x
+
 # file size               (blocks, -f) unlimited
 # pending signals                 (-i) 63562
 # max locked memory       (kbytes, -l) 64
-# max memory size         (kbytes, -m) unlimited
 # open files                      (-n) 1024
 # pipe size            (512 bytes, -p) 8
 # POSIX message queues     (bytes, -q) 819200
 # real-time priority              (-r) 0
 # stack size              (kbytes, -s) 8192
-# cpu time               (seconds, -t) unlimited
 # max user processes              (-u) 63562
-# virtual memory          (kbytes, -v) unlimited
 # file locks                      (-x) unlimited
 
 # Options:
